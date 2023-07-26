16540f6a3e2fcdee8901ad33da8c656f6bafc171
cseaotter <42598226+cseaotter@users.noreply.github.com>
2023-07-26 11:41:29 -0400

    post and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,19 @@
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">My Blog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.88847xw:1xh;center,top&resize=1200:*" alt=""/>
-        </div>
-        <div className="texts">
-          <h2>Cutest Cat of the Year</h2>
-          <p className="info">
-            <a className="author">Carol Gao</a>
-            <time>2023-07-25 17:54</time>
-          </p>
-          <p className="summary">Here are the cutest cat of the year</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.88847xw:1xh;center,top&resize=1200:*" alt=""/>
-        </div>
-        <div className="texts">
-          <h2>Cutest Cat of the Year</h2>
-          <p className="info">
-            <a className="author">Carol Gao</a>
-            <time>2023-07-25 17:54</time>
-          </p>
-          <p className="summary">Here are the cutest cat of the year</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.88847xw:1xh;center,top&resize=1200:*" alt=""/>
-        </div>
-        <div className="texts">
-          <h2>Cutest Cat of the Year</h2>
-          <p className="info">
-            <a className="author">Carol Gao</a>
-            <time>2023-07-25 17:54</time>
-          </p>
-          <p className="summary">Here are the cutest cat of the year</p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage/>}/>
+      </Route>
+    </Routes>
   );
 }
 
---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return (
+        <div className="post">
+        <div className="image">
+          <img src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.88847xw:1xh;center,top&resize=1200:*" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>Cutest Cat of the Year</h2>
+          <p className="info">
+            <a className="author">Carol Gao</a>
+            <time>2023-07-25 17:54</time>
+          </p>
+          <p className="summary">Here are the cutest cat of the year</p>
+        </div>
+      </div>
+    );
+}
