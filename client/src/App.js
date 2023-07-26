d4ddf856996da78f167c327c22198c65637283bd
cseaotter <42598226+cseaotter@users.noreply.github.com>
2023-07-26 10:17:50 -0400

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">My Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.88847xw:1xh;center,top&resize=1200:*" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>Cutest Cat of the Year</h2>
+          <p className="info">
+            <a className="author">Carol Gao</a>
+            <time>2023-07-25 17:54</time>
+          </p>
+          <p className="summary">Here are the cutest cat of the year</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.88847xw:1xh;center,top&resize=1200:*" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>Cutest Cat of the Year</h2>
+          <p className="info">
+            <a className="author">Carol Gao</a>
+            <time>2023-07-25 17:54</time>
+          </p>
+          <p className="summary">Here are the cutest cat of the year</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.88847xw:1xh;center,top&resize=1200:*" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>Cutest Cat of the Year</h2>
+          <p className="info">
+            <a className="author">Carol Gao</a>
+            <time>2023-07-25 17:54</time>
+          </p>
+          <p className="summary">Here are the cutest cat of the year</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
