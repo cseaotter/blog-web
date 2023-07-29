29561db3bd52fd1ff7f3f3d8442378ea574defc5
cseaotter <42598226+cseaotter@users.noreply.github.com>
2023-07-29 14:59:09 -0400

    login credential

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -11,6 +11,7 @@ export default function RegisterPage() {
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
     });
+    
     if (response.status !== 200) {
         alert('registration failed');
     } else {
