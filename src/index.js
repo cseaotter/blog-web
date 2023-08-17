7a71f574abcf48da80a7b3ba586b358840b5e4dd
cseaotter <42598226+cseaotter@users.noreply.github.com>
2023-08-16 23:58:41 -0400

    disable dev tools

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
 import { UserContextProvider } from "./UserContext";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
